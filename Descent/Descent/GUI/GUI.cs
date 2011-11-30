4a26ba3f0d9344009508a75c6b5d60af737b11af
nezbo <emilovicmail@gmail.com>
2011-11-30 20:19:02 +0100

    guielements and stuff

---
+++ Descent/Descent/GUI/GUI.cs
@@ -0,0 +1,71 @@
+﻿namespace Descent.GUI
+{
+    using Microsoft.Xna.Framework.Graphics;
+
+    /// <summary>
+    /// Handles the visual aspects of the game and input
+    /// from the user.
+    /// </summary>
+    /// <author>
+    /// Emil Juul Jacobsen
+    /// </author>
+    class GUI
+    {
+        private SpriteBatch draw; // the drawing surface
+
+        /// <summary>
+        /// The three layers of the GUI:
+        /// 0: The board.
+        /// 1: The state.
+        /// 2: The chat and info
+        /// </summary>
+        private GUIElement[] layers;
+
+        /// <summary>
+        /// Creates a Graphical User Interface.
+        /// </summary>
+        /// <param name="draw">The surface to draw on.</param>
+        public GUI(SpriteBatch draw)
+        {
+            this.draw = draw;
+
+            layers = new GUIElement[3];
+        }
+
+        /// <summary>
+        /// Draws the GUI to the screen.
+        /// </summary>
+        public void Draw()
+        {
+            draw.Begin();
+            foreach (GUIElement element in layers)
+            {
+                element.Draw(draw);
+            }
+            draw.End();
+        }
+
+        /// <summary>
+        /// Lets the topmost element on the screen
+        /// react to the given click.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the click</param>
+        /// <param name="y">The y-coordinate of the click</param>
+        public void HandleClick(int x, int y)
+        {
+            for (int i = layers.Length - 1; i >= 0; i--)
+            {
+                if (layers[i].HandleClick(x, y)) break;
+            }
+        }
+
+        /// <summary>
+        /// Changes the GUIElement of the state layer.
+        /// </summary>
+        /// <param name="state">The new GUIElement of the State layer</param>
+        public void ChangeStateGUI(GUIElement state)
+        {
+            layers[1] = state;
+        }
+    }
+}
---
+++ Descent/Descent/GUI/GUIElement.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Descent.GUI
+{
+    using System.Collections.ObjectModel;
+    using Microsoft.Xna.Framework;
+
+    class GUIElement
+    {
+        //private static StateManager manager = Player.Instance.StateManager;
+
+        private Rectangle bound;
+        public Rectangle Bound { get { return bound; } }
+
+        private string name;
+        public string Name { get { return name; } }
+
+        private Collection<GUIElement> children;
+        private Dictionary<Drawable, Point> visuals;
+
+        /// <summary>
+        /// Creates a GUIElement with the given boundaries.
+        /// </summary>
+        /// <param name="name">The name of the element</param>
+        /// <param name="x">The x-coordinate of the upper-left corner</param>
+        /// <param name="y">The y-coordinate of the upper-left corner</param>
+        /// <param name="width">The width of the element</param>
+        /// <param name="height">The height of the element</param>
+        public GUIElement(string name, int x, int y, int width, int height)
+        {
+            this.name = name;
+            bound = new Rectangle(x, y, width, height);
+            children = new Collection<GUIElement>();
+            visuals = new Dictionary<Drawable, Point>();
+        }
+
+        /// <summary>
+        /// Checks if the given point is within the boundaries of the GUIElement.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the point</param>
+        /// <param name="y">The y-coordinate of the point</param>
+        /// <returns></returns>
+        public bool HasPoint(int x, int y)
+        {
+            return bound.Contains(x, y);
+        }
+
+        /// <summary>
+        /// Determines if a click is within the GUIElement and acts
+        /// upon the click if possible.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the click</param>
+        /// <param name="y">The y-coordinate of the click</param>
+        public bool HandleClick(int x, int y)
+        {
+            if (this.HasPoint(x, y))
+            {
+                foreach (GUIElement e in children)
+                {
+                    if (e.HandleClick(x, y))
+                    {
+                        return true;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddClickEvent(Action<StateManager> action)
+        {
+
+        }
+    }
+}
