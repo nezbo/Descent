f60ab5327b6909703159bac3a899a37f8501124e
nezbo <emilovicmail@gmail.com>
2011-11-30 20:57:26 +0100

    added drawable and finished guielement

--- Descent/Descent/GUI/GUIElement.cs
+++ Descent/Descent/GUI/GUIElement.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Descent.GUI
 {
     using System.Collections.ObjectModel;
     using Microsoft.Xna.Framework;
 
+    /// <summary>
+    /// A single element of the user interface that, itself, 
+    /// can contain sub-elements
+    /// </summary>
+    /// <author>
+    /// Emil Juul Jacobsen
+    /// </author>
     class GUIElement
     {
-        //private static StateManager manager = Player.Instance.StateManager;
+        private static StateManager manager = Player.Instance.StateManager;
 
         private Rectangle bound;
         public Rectangle Bound { get { return bound; } }
@@ -17,7 +25,9 @@ namespace Descent.GUI
         public string Name { get { return name; } }
 
         private Collection<GUIElement> children;
-        private Dictionary<Drawable, Point> visuals;
+
+        private Dictionary<Drawable, Vector2> visuals;
+        private Action<StateManager> onClick = null;
 
         /// <summary>
         /// Creates a GUIElement with the given boundaries.
@@ -32,7 +42,7 @@ namespace Descent.GUI
             this.name = name;
             bound = new Rectangle(x, y, width, height);
             children = new Collection<GUIElement>();
-            visuals = new Dictionary<Drawable, Point>();
+            visuals = new Dictionary<Drawable, Vector2>();
         }
 
         /// <summary>
@@ -54,8 +64,10 @@ namespace Descent.GUI
         /// <param name="y">The y-coordinate of the click</param>
         public bool HandleClick(int x, int y)
         {
+            // is it within me
             if (this.HasPoint(x, y))
             {
+                // is it within any of my children
                 foreach (GUIElement e in children)
                 {
                     if (e.HandleClick(x, y))
@@ -63,17 +75,69 @@ namespace Descent.GUI
                         return true;
                     }
                 }
+
+                // ok, its within me, ill handle it!
+                if (onClick != null)
+                {
+                    onClick(manager);
+                }
                 return true;
             }
             else
             {
+                // nope, wasnt me :(
                 return false;
             }
         }
 
-        public void AddClickEvent(Action<StateManager> action)
+        /// <summary>
+        /// Replace (if any) the current On Click action with
+        /// the one given.
+        /// </summary>
+        /// <param name="action">The new On Click action</param>
+        public void AddClickAction(Action<StateManager> action)
         {
+            this.onClick = action;
+        }
 
+        /// <summary>
+        /// Adds the given GUIElement to the set of children within
+        /// this one.
+        /// </summary>
+        /// <param name="child">The new child</param>
+        public void AddChild(GUIElement child)
+        {
+            children.Add(child);
+        }
+
+        /// <summary>
+        /// Add the drawable to be displayed at the given position
+        /// in this GUIElement.
+        /// </summary>
+        /// <param name="visual">The drawable to display</param>
+        /// <param name="position">Where the upper-left corner of the drawable should be</param>
+        public void AddDrawable(Drawable visual, Vector2 position)
+        {
+            visuals.Add(visual, position);
+        }
+
+        /// <summary>
+        /// Draws this GUIElement and then all children on top of it
+        /// </summary>
+        /// <param name="draw">The SpriteBatch to draw on</param>
+        public void Draw(SpriteBatch draw)
+        {
+            // draw myself
+            foreach (Drawable d in visuals.Keys)
+            {
+                draw.Draw(d.Texture, visuals[d], Color.White);
+            }
+
+            // draw the children on top
+            foreach (GUIElement e in children)
+            {
+                e.Draw(draw);
+            }
         }
     }
 }
