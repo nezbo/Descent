3b26ed559f73cfc8b2bc2ca80188011574cf20b5
Jonas Breindahl <jobre@itu.dk>
2011-12-01 16:57:16 +0100

    Added board, square and figure of model

---
+++ Descent/Descent/Model/Board/Square.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Square.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Descent.Model.Board
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// A single square of the board
+    /// </summary>
+    public class Square
+    {
+        #region Square.NONE
+
+        private static Square none;
+        public static Square NONE
+        {
+            get
+            {
+                return none ?? (none = new Square());
+            }
+        }
+        #endregion SQUARE.NONE
+
+        #region FIELDS
+
+        public Marker Marker { get; set; }
+
+        public Figure Figure { get; set; }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        #endregion
+
+        #region METHODS
+
+        #endregion
+
+    }
+}
