a50ea2645aa7679aaa2b2c27c13c256f2463a4e2
Martin Marcher <marcher1210@gmail.com>
2011-12-01 14:19:14 +0100

    StateMachine finished.

---
+++ Descent/Descent/State/State.cs
@@ -0,0 +1,7 @@
+﻿namespace Descent.State
+{
+    public enum State
+    {
+        HeroPartyTurn, HeroPartyInitiation, DrawHeroCard, DrawSkillCards, WaitForTradeCard, TradeCard, BuyEquipment
+    }
+}
--- Descent/Descent/State/StateManager.cs
+++ Descent/Descent/State/StateManager.cs
@@ -10,11 +10,28 @@ namespace Descent.State
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+using System.Diagnostics.Contracts;
 
     /// <summary>
-    /// TODO: Write StateManager class
+    /// The handler of all states. Knows about the current state and what to do next.
     /// </summary>
+    /// <author>Martin Marcher</author>
     public class StateManager
     {
+        private StateMachine stateMachine;
+
+        public State CurrentState { get { return stateMachine.CurrentState; } }
+
+        public StateManager()
+        {
+            stateMachine = new StateMachine(new State[] {State.HeroPartyTurn, State.HeroPartyInitiation});
+        }
+
+        public State[] PreviousStates(int count)
+        {
+            Contract.Requires(count > 0);
+            Contract.Ensures(Contract.Result<State[]>().Length <= count);
+            return stateMachine.PreviousStates(count);
+        }
     }
 }
