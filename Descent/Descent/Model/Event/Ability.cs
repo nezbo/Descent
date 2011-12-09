76ae0d5b726e1210b4cf89f8f9473c4dab292439
Jonas Breindahl <jobre@itu.dk>
2011-12-09 12:52:53 +0100

    Changes to chest, fullmodel etc.

--- Descent/Descent/Model/Event/Ability.cs
+++ Descent/Descent/Model/Event/Ability.cs
@@ -25,14 +25,26 @@ namespace Descent.Model.Event
             return new Ability(false, isTrue);
         }
 
+        private static bool GetTrigger(string trigger)
+        {
+            bool triggerFunc;
+
+            if (trigger.StartsWith("WhenAttacking"))
+            {
+                //triggerFunc = IfAttacking();
+            }
+
+            return false;
+        }
+
         private static bool isTrue()
         {
             return true;
         }
 
-        private static bool IfAttacking()
+        private static Func<bool> IfAttacking()
         {
-            return false;
+            return null;
         }
         #endregion
 
