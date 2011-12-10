d990b77908a26617ebcd469a3ab8df196b3f844e
nezbo <emilovicmail@gmail.com>
2011-12-10 23:10:37 +0100

    dice element done

--- Descent/Descent/GUI/SubElements/DicesElement.cs
+++ Descent/Descent/GUI/SubElements/DicesElement.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Descent.Messaging.Events;
 using Descent.Model;
 using Microsoft.Xna.Framework;
 
@@ -7,6 +8,8 @@ namespace Descent.GUI.SubElements
     class DicesElement : GUIElement
     {
         private static readonly int Spacing = 10;
+        private int diceWidth;
+        private int perLine;
 
         public DicesElement(Game game, Dice[] dices, int x, int y, int width, int height)
             : base(game, "dice", x, y, width, height)
@@ -14,8 +17,8 @@ namespace Descent.GUI.SubElements
             // determine how many can be displayed on a line
             if (dices.Length > 0)
             {
-                int diceWidth = dices[0].Texture.Width + Spacing;
-                int perLine = 1;
+                diceWidth = dices[0].Texture.Width + Spacing;
+                perLine = 1;
                 int widthLeft = Bound.Width - dices[0].Texture.Width - 2 * Spacing;
 
                 while (widthLeft > diceWidth)
@@ -29,11 +32,22 @@ namespace Descent.GUI.SubElements
                 int number = 0;
                 foreach (EDice diceType in Enum.GetValues(typeof(EDice)))
                 {
-                    foreach (Dice die in dices)
+                    for (int d = 0; d < dices.Length; d++)
                     {
                         if (die.Color == diceType)
                         {
-                            this.AddDrawable(this.Name, die, new Vector2(Bound.X + Spacing + number * die.Texture.Width + number * Spacing, Bound.Y + Spacing + line * die.Texture.Height + line * Spacing));
+                            Dice die = dices[d];
+                            GUIElement diceBox = new GUIElement(game, "dice", Bound.X + Spacing + number * die.Texture.Width + number * Spacing,
+                                                                Bound.Y + Spacing + line * die.Texture.Height + line * Spacing,
+                                                                die.Texture.Width, die.Texture.Height);
+                            diceBox.AddDrawable(diceBox.Name, die, new Vector2(0, 0));
+                            diceBox.SetClickAction(diceBox.Name, (n, g) =>
+                                                                     {
+                                                                         n.EventManager.QueueEvent(EventType.DiceClicked, new DiceEventArgs(d, die.SideIndexBound.X + Spacing + number * die.Texture.Width + number * Spacing, Bound.Y + Spacing + line * die.Texture.Height + line * Spacing));
+                                                                     });
+                            this.AddChild(diceBox);
+
+
                             number++;
 
                             if (number >= perLine)
