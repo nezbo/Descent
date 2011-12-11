fca2992e66240a01c0a1d8fe2c0c04c096e8fd03
nezbo <emilovicmail@gmail.com>
2011-12-11 21:11:08 +0100

    monster summary done, but abilities dont print the tostring?

--- Descent/Descent/GUI/SubElements/MonsterSummary.cs
+++ Descent/Descent/GUI/SubElements/MonsterSummary.cs
@@ -1,4 +1,10 @@
-﻿using Descent.Model.Player.Figure;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Descent.Model;
+using Descent.Model.Event;
+using Descent.Model.Player;
+using Descent.Model.Player.Figure;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,21 +12,85 @@ namespace Descent.GUI.SubElements
 {
     class MonsterSummary : GUIElement
     {
+        private static Dictionary<EDice, Texture2D> dicetinary;
+
         public MonsterSummary(Game game, int x, int y, Monster monster)
             : base(game, "monster summary", x, y, 125, 175)
         {
             SetBackground("boxbg");
 
-            // images
+            if (dicetinary == null) LoadDiceTextures(game);
+
+            // top images
             AddDrawable(this.Name, new Image(game.Content.Load<Texture2D>("Images/Other/movement-small")), new Rectangle(Bound.X, Bound.Y + 25, 25, 25));
             AddDrawable(this.Name, new Image(game.Content.Load<Texture2D>("Images/Other/health-small")), new Rectangle(Bound.X + Bound.Width / 2, Bound.Y + 25, 25, 25));
             AddDrawable(this.Name, new Image(game.Content.Load<Texture2D>("Images/Other/armor-small")), new Rectangle(Bound.X, Bound.Y + 55, 25, 25));
 
-            // text
+            // top text
             AddText(this.Name, monster.Name, new Vector2(0, 0));
             AddText(this.Name, monster.MovementLeft + "/" + monster.Speed, new Vector2(30, 25));
             AddText(this.Name, monster.Health + "/" + monster.MaxHealth, new Vector2(30 + Bound.Width / 2, 25));
             AddText(this.Name, "" + monster.Armor, new Vector2(30, 55));
+
+            // icons part
+            Texture2D attackType;
+            switch (monster.AttackType)
+            {
+                case EAttackType.MAGIC:
+                    {
+                        attackType = game.Content.Load<Texture2D>("Images/Other/magic-icon");
+                        break;
+                    }
+
+                case EAttackType.RANGED:
+                    {
+                        attackType = game.Content.Load<Texture2D>("Images/Other/ranged-icon");
+                        break;
+                    }
+                default:
+                    {
+                        attackType = game.Content.Load<Texture2D>("Images/Other/melee-icon");
+                        break;
+                    }
+            }
+
+            AddDrawable(this.Name, new Image(attackType), new Rectangle(Bound.X + 5, Bound.Y + 85, 20, 20));
+            int xPos = Bound.X + 35;
+            int yPos = Bound.Y + 85;
+            foreach (EDice dice in monster.DiceForAttack.Select(n => n.Color))
+            {
+                if (xPos + 20 >= (Bound.X + Bound.Width))
+                {
+                    yPos += 20;
+                    xPos = Bound.X + 35;
+                }
+
+                AddDrawable(this.Name, new Image(dicetinary[dice]), new Rectangle(xPos, yPos, 20, 20));
+                xPos += 20;
+            }
+
+            // abilities
+            StringBuilder builder = new StringBuilder();
+            foreach (Ability a in monster.Abilities)
+            {
+                builder.Append(a.ToString());
+                builder.Append(',');
+            }
+            if (builder.Length > 2) builder.Remove(builder.Length - 2, 1);
+
+            AddText(this.Name, builder.ToString(), new Vector2(5, yPos + 25 - Bound.Y));
+        }
+
+        private void LoadDiceTextures(Game game)
+        {
+            dicetinary = new Dictionary<EDice, Texture2D>();
+
+            dicetinary.Add(EDice.B, game.Content.Load<Texture2D>("Images/Other/dice-black-icon"));
+            dicetinary.Add(EDice.G, game.Content.Load<Texture2D>("Images/Other/dice-green-icon"));
+            dicetinary.Add(EDice.R, game.Content.Load<Texture2D>("Images/Other/dice-red-icon"));
+            dicetinary.Add(EDice.U, game.Content.Load<Texture2D>("Images/Other/dice-blue-icon"));
+            dicetinary.Add(EDice.W, game.Content.Load<Texture2D>("Images/Other/dice-white-icon"));
+            dicetinary.Add(EDice.Y, game.Content.Load<Texture2D>("Images/Other/dice-yellow-icon"));
         }
     }
 }
