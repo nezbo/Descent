262bb0db1e84b80d92bb207d3c4ff134274f44bb
nezbo <emilovicmail@gmail.com>
2011-12-11 21:50:19 +0100

    coins in own player interface

--- Descent/Descent/GUI/SubElements/OverlordElement.cs
+++ Descent/Descent/GUI/SubElements/OverlordElement.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Descent.GUI.SubElements
 {
@@ -9,6 +10,7 @@ namespace Descent.GUI.SubElements
             : base(game, "hero", 0, 0, (int)(game.GraphicsDevice.Viewport.Width * (3 / 4.0)), game.GraphicsDevice.Viewport.Height)
         {
             SetDrawBackground(false);
+            AddDrawable(Name, new Image(game.Content.Load<Texture2D>("Images/Heroes/BIG-0")), new Rectangle(Bound.X, Bound.Y + Bound.Height - 200, 200, 200));
         }
     }
 }
