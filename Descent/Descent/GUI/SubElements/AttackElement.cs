77c7b9b6e90e23d55633763f92a7bfb771b0019e
nezbo <emilovicmail@gmail.com>
2011-12-11 15:50:57 +0100

    surge abilities gui

--- Descent/Descent/GUI/SubElements/AttackElement.cs
+++ Descent/Descent/GUI/SubElements/AttackElement.cs
@@ -1,4 +1,6 @@
-﻿using Descent.Model.Event;
+﻿using System.Collections.Generic;
+using Descent.Model.Event;
+using Descent.Model.Player.Figure.HeroStuff;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -16,13 +18,23 @@ namespace Descent.GUI.SubElements
             AddChild(new DicesElement(game, attack.DiceForAttack.ToArray(), Bound.X + (int)(Bound.Width * 0.4), Bound.Y, (int)(Bound.Width * 0.6), Bound.Height / 2));
 
             //TODO: some way to spend surges
+            List<SurgeAbility> surge = attack.SurgeAbilities;
+
+            int yPos = Bound.Height / 2;
+            int height = 100;
+            bool left = true;
+
+            for (int i = 0; i < surge.Count; i++)
+            {
+
+            }
         }
 
         public override void Draw(SpriteBatch draw)
         {
             base.Draw(draw);
 
-            draw.DrawString(GUI.Font, "Test", new Vector2(Bound.X + 5, Bound.Y + 5), Color.Black);
+            draw.DrawString(GUI.Font, attack.ToString(), new Vector2(Bound.X + 5, Bound.Y + 5), Color.Black);
         }
     }
 }
