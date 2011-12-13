17256a93246661350256f9ff22e0f7a3b1f5d9a9
Martin Marcher <marcher1210@gmail.com>
2011-12-13 20:06:31 +0100

    Woops

--- Descent/Descent/State/GameState.cs
+++ Descent/Descent/State/GameState.cs
@@ -170,7 +170,7 @@
         public void RemoveFromUnequippedEquipment(int playerId, Equipment equipment)
         {
             Contract.Requires(playerId > 0);
-            Contract.Ensures(UnequippedEquipment(playerId).Contains(equipment));
+            Contract.Ensures(!UnequippedEquipment(playerId).Contains(equipment));
 
             unequippedEquipment[playerId].Remove(equipment);
         }
